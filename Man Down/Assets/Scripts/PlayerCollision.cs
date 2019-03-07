a66c0a0c22c051d38a22b9bb6b7b6b0a51f427ae
Junaid Khan <psyjk2@nottingham.ac.uk>
2019-03-07 12:45:03 +0000

    Collision Detection Added
    Collision detection is added. When the ball collides with a wall, it flashes white for 2 seconds prior to reverting to its original colour. Works for all walls.

---
+++ Man Down/Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+
+    private void OnCollisionEnter(UnityEngine.Collision collisionInfo)
+    {   
+        if(collisionInfo.gameObject.name == "eastWallYellow" || collisionInfo.gameObject.name == "southWallYellow")
+        {
+            //Debug.Log(collisionInfo.collider.name);
+            Debug.Log("Yellow wall hit");
+        }
+        if (collisionInfo.gameObject.name == "westWallGreen" || collisionInfo.gameObject.name == "northWallGreen")
+        {
+            //Debug.Log(collisionInfo.collider.name);
+            Debug.Log("Green wall hit");
+        }
+        if (collisionInfo.gameObject.name == "eastWallBlue" || collisionInfo.gameObject.name == "northWallBlue")
+        {
+            //Debug.Log(collisionInfo.collider.name);
+            Debug.Log("Blue wall hit");
+        }
+        if (collisionInfo.gameObject.name == "westWallOrange" || collisionInfo.gameObject.name == "southWallOrange")
+        {
+            //Debug.Log(collisionInfo.collider.name);
+            Debug.Log("Orange wall hit");
+        }
+        //yield return new WaitForSeconds(2);
+    }
+
+}
---
+++ Man Down/Assets/Scripts/WallCollision.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallCollision : MonoBehaviour
+{   
+    GameObject eWY,sWY,wWG,nWG,eWB,nWB,wWO,sWO;
+
+    private IEnumerator OnCollisionEnter(UnityEngine.Collision collisionInfo)
+    {
+        if (collisionInfo.gameObject.name == "westWallGreen" || collisionInfo.gameObject.name == "northWallGreen")
+        {
+            Color green = new Color32(26, 236, 23, 1);
+            wWG = GameObject.Find("westWallGreen");
+            nWG = GameObject.Find("northWallGreen");
+            collisionInfo.gameObject.GetComponent<Renderer>().material.color = Color.white;
+            yield return new WaitForSeconds(2);
+            wWG.gameObject.GetComponent<Renderer>().material.color = green;
+            nWG.gameObject.GetComponent<Renderer>().material.color = green;
+        }
+        if (collisionInfo.gameObject.name == "eastWallYellow" || collisionInfo.gameObject.name == "southWallYellow")
+        {
+            Color yellow = new Color32(222, 231, 19, 1);
+            eWY = GameObject.Find("eastWallYellow");
+            sWY = GameObject.Find("southWallYellow");
+            collisionInfo.gameObject.GetComponent<Renderer>().material.color = Color.white;
+            yield return new WaitForSeconds(2);
+            eWY.gameObject.GetComponent<Renderer>().material.color = yellow;
+            sWY.gameObject.GetComponent<Renderer>().material.color = yellow;
+
+        }
+        if (collisionInfo.gameObject.name == "eastWallBlue" || collisionInfo.gameObject.name == "northWallBlue")
+        {
+            Color blue = new Color32(44, 80, 207, 1);
+            eWB = GameObject.Find("eastWallBlue");
+            nWB = GameObject.Find("northWallBlue");
+            collisionInfo.gameObject.GetComponent<Renderer>().material.color = Color.white;
+            yield return new WaitForSeconds(2);
+            eWB.gameObject.GetComponent<Renderer>().material.color = blue;
+            nWB.gameObject.GetComponent<Renderer>().material.color = blue;
+        }
+        if (collisionInfo.gameObject.name == "westWallOrange" || collisionInfo.gameObject.name == "southWallOrange")
+        {
+            Color orange = new Color32(248, 151, 11, 2);
+            wWO = GameObject.Find("westWallOrange");
+            sWO = GameObject.Find("southWallOrange");
+            collisionInfo.gameObject.GetComponent<Renderer>().material.color = Color.white;
+            yield return new WaitForSeconds(2);
+            wWO.gameObject.GetComponent<Renderer>().material.color = orange;
+            sWO.gameObject.GetComponent<Renderer>().material.color = orange;
+        }
+    }
+}
